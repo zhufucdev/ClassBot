98c89e06b1151d64de4bcac32bd47aeb4ab5514d
zhufucdev <1095291434@qq.com>
2022-03-15 22:50:26 +0000

    Fixed a date calculation error

--- src/main/kotlin/data/SignUpRecord.kt
+++ src/main/kotlin/data/SignUpRecord.kt
@@ -4,6 +4,7 @@ import com.zhufucdev.serialization.defaultZone
 import net.mamoe.mirai.contact.Group
 import net.mamoe.mirai.contact.Member
 import java.time.Instant
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class SignUpRecord(classmate: Long, timestamp: Instant) : Record(classmate, timestamp) {
@@ -13,8 +14,8 @@ class SignUpRecord(classmate: Long, timestamp: Instant) : Record(classmate, time
         fun hasSignedToday(classmate: Long, group: Group) = getTodayRecord(classmate, group) != null
 
         fun getTodayRecord(classmate: Long, group: Group) = Database[group].firstOrNull {
-            it is SignUpRecord && it.classmate == classmate && LocalDateTime.ofInstant(it.timestamp, defaultZone)
-                .toLocalDate().isEqual(LocalDateTime.now().atZone(defaultZone).toLocalDate())
+            it is SignUpRecord && it.classmate == classmate
+                    && LocalDate.ofInstant(it.timestamp, defaultZone).isEqual(LocalDate.now(defaultZone))
         }
     }
 }
